c7ab958a14ad636da62741896a13b30c90fda418
fabien <fabien.andrian@gmail.com>
2022-10-28 23:32:46 +0200

    navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
+})
+
+export class NavbarComponent {
+    logo = "logo"
+}
